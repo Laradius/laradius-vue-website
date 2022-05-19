66abd2c9b7a971799014cadba9f1d02f0fb1db91
Laradius <loganl33t@gmail.com>
2022-05-19 19:19:24 +0200

    More UI work, components to refactor

--- src/App.vue
+++ src/App.vue
@@ -40,14 +40,38 @@
 
 <style>
 :root {
+  --site-background: #303030;
+  --paper-background: #424242;
   --primary: #8c3bc4;
   --primary-text: #fff;
+  --primary-hover: rgba(255, 255, 255, 0.9);
+  --primary-secondary-text: rgba(255, 255, 255, 0.7);
   --secondary: #73c43b;
   --secondary-text: rgba(0, 0, 0, 0.87);
 }
 
+body {
+  background-color: var(--site-background) !important;
+}
+
 .main-nav {
   background-color: var(--primary);
-  color: var(--primary-text);
+  color: var(--primary-secondary-text);
+}
+
+.nav-link {
+  padding-left: 10px !important;
+  padding-right: 10px !important;
+  display: inline-block !important;
+  color: var(--primary-text) !important;
+  transition: unset !important;
+}
+
+.nav-link:hover {
+  color: var(--primary-hover) !important;
+}
+
+.router-link-exact-active {
+  border-bottom: 2px solid var(--secondary);
 }
 </style>
