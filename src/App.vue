60664d1f9b2a67e180ca7b8d0e27c955a3a4bdcb
Laradius <loganl33t@gmail.com>
2022-05-19 18:47:01 +0200

    Basic UI work

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container-fluid main-nav text-center">
+    <a href="#">
+      <img src="./assets/logo.png" alt="logo" height="80" />
+    </a>
+
+    <nav id="nav" class="navbar navbar-expand-lg navbar-dark">
+      <div class="justify-content-center container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          class="justify-content-center text-center collapse navbar-collapse"
+          id="navbarNav"
+        >
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about">About</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </div>
-  <router-view/>
+
+  <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+:root {
+  --primary: #8c3bc4;
+  --primary-text: #fff;
+  --secondary: #73c43b;
+  --secondary-text: rgba(0, 0, 0, 0.87);
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.main-nav {
+  background-color: var(--primary);
+  color: var(--primary-text);
 }
 </style>
